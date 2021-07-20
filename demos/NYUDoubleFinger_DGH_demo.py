f659d0390707c6e999862037ad384c4bd314ec8b
Jerry <jerrywuzijie@gmail.com>
2021-07-20 14:38:00 -0400

    implement safety control for real robot

--- demos/NYUDoubleFinger_DGH_demo.py
+++ demos/NYUDoubleFinger_DGH_demo.py
@@ -5,6 +5,7 @@ import os
 import time
 import sys
 from threading import current_thread
+
 import numpy as np
 import matplotlib.pylab as plt
 import pinocchio as pin
@@ -19,16 +20,18 @@ import dynamic_graph_manager_cpp_bindings
 
 
 # param
-PD_0 =np.array([3.]*3)
+PD_0 = np.array([1.]*3)
 PD_1 = np.array([.1]*3)
-VEL_0 = 1
+VEL_0 = 3
 VEL_1 = np.array([.3]*3)
 IMP_0 = np.diag([50, 50, 10])
 IMP_1 = np.diag([5, 5, 0])
-IMP_H_0 = np.diag([10]*3)
-IMP_H_1 = np.diag([5]*3)
+IMP_H_0 = np.diag([50]*3)
+IMP_H_1 = np.diag([10]*3)
 
 # function to calculate robot kinematics ----------------------
+
+
 def cal_pose(pin_robot, id_ee):
     '''
     calculate position matrix (transition and rotation)
@@ -172,7 +175,7 @@ class ImpedanceControl(Control):
 
 # controllers for running ----------------------
 class Controller:
-    def __init__(self, head, id_ee, param0, param1, control, dt=0.001, hold_time=5):
+    def __init__(self, head, id_ee, param0, param1, control, dt=0.001, hold_time=2):
         # setup head
         self.head = head
         # pos and vel is reference, will update automatically
@@ -324,19 +327,24 @@ def choose_controller(finger, control, head, id):
     '''
     choose controller based on finger, return controller and q, dq
     '''
+    center = [0.0506947, 0.0594499, 0.05]  # init position
+    center[0] += 0.1
+    center[1] += 0.05  # avoid two robot touching
+    center[2] += 0.1
+    radius = 0.02
+
     if finger == 0:  # first finger
         if control == 0:
             # finger0 PD
-            ctrl = PDController(head, id, PD_0, PD_1, np.array([0, 0, np.pi/3]))
+            ctrl = PDController(head, id, PD_0, PD_1,
+                                np.array([0, 0, np.pi/3]))
         elif control == 1:
             # finger0 velocity
-            center = [-0.0506, -0.05945, 0.05]
-            center[0] -= 0.1
-            center[1] -= 0.05  # avoid two robot touching
-            center[2] += 0.15
-            radius = 0.04
+            center0 = center
+            center0[0] = -center[0]
+            center0[1] = -center[1]
             ctrl = VelocityController(
-                head, id, VEL_0, VEL_1, center, radius, np.pi*3)
+                head, id, VEL_0, VEL_1, center0, radius, np.pi*3)
         else:
             # # finger0 impedance
             ctrl = ImpedanceController(
@@ -344,16 +352,13 @@ def choose_controller(finger, control, head, id):
     else:  # second finger
         if control == 0:
             # finger1 PD
-            ctrl = PDController(head, id, PD_0, PD_1, np.array([0, 0, np.pi/3]))
+            ctrl = PDController(head, id, PD_0, PD_1,
+                                np.array([0, 0, np.pi/3]))
         elif control == 1:
             # finger1 velocity
-            center = [0.0506947, 0.0594499, 0.05]
-            center[0] += 0.1
-            center[1] += 0.05  # avoid two robot touching
-            center[2] += 0.15
-            radius = 0.04
+            center1 = center
             ctrl = VelocityController(
-                head, id, VEL_0, VEL_1, center, radius, -np.pi*3)
+                head, id, VEL_0, VEL_1, center1, radius, -np.pi*3)
         else:
             # finger1 impedance
             ctrl = ImpedanceController(
@@ -361,147 +366,98 @@ def choose_controller(finger, control, head, id):
     return ctrl
 
 
-def main_sim(sim_time, finger0_controller=0, finger1_controller=0):
-    # init BulletEnv and setup robot ----------------------
-    bullet_env = BulletEnv()  # BulletEnvWithGround()
+if __name__ == '__main__':
+    # setup some parameters
+    SIMULATION = False
+    FINGER0_CONTROLLER = 0
+    FINGER1_CONTROLLER = 2
+    FINGER0_ONLY = False
+    ID0 = 'finger0_lower_to_tip_joint'
+    ID1 = 'finger1_lower_to_tip_joint'
+    DT = 0.001
 
-    # Create a robot instance. This initializes the simulator as well.
-    config0 = NYUFingerDoubleConfig0()
-    config1 = NYUFingerDoubleConfig1()
-    finger0 = NYUFingerRobot(config=config0)
-    finger1 = NYUFingerRobot(config=config1)
-    id0 = 'finger0_lower_to_tip_joint'
-    id1 = 'finger1_lower_to_tip_joint'
-    bullet_env.add_robot(finger0)
-    bullet_env.add_robot(finger1)
+    # setup head
+    if SIMULATION:
+        # init BulletEnv and setup robot ----------------------
+        bullet_env = BulletEnv()  # BulletEnvWithGround()
 
-    # setup simulation and thread_head ----------------------
-    dt = 0.001
+        # Create a robot instance. This initializes the simulator as well.
+        config0 = NYUFingerDoubleConfig0()
+        config1 = NYUFingerDoubleConfig1()
+        finger0 = NYUFingerRobot(config=config0)
+        finger1 = NYUFingerRobot(config=config1)
+        id0 = 'finger0_lower_to_tip_joint'
+        id1 = 'finger1_lower_to_tip_joint'
+        bullet_env.add_robot(finger0)
+        bullet_env.add_robot(finger1)
+        head0 = SimHead(finger0, vicon_name='solo12', with_sliders=False)
+        head1 = SimHead(finger1, vicon_name='solo12', with_sliders=False)
+        head_dict = {'default': head0} if FINGER0_ONLY else {
+            'finger0': head0, 'finger1': head1}
+    else:
+        # Create the dgm communication and instantiate the controllers.
+        path0 = os.path.join(NYUFingerDoubleConfig0().dgm_yaml_dir,
+                             'dgm_parameters_nyu_finger_double_0.yaml')
+        path1 = os.path.join(NYUFingerDoubleConfig1().dgm_yaml_dir,
+                             'dgm_parameters_nyu_finger_double_1.yaml')
+        head0 = dynamic_graph_manager_cpp_bindings.DGMHead(path0)
+        head1 = dynamic_graph_manager_cpp_bindings.DGMHead(path1)
+        head0.read()
+        head1.read()
+        head_dict = {'default': head0} if FINGER0_ONLY else {
+            'finger0': head0, 'finger1': head1}
 
-    head0 = SimHead(finger0, vicon_name='solo12', with_sliders=False)
-    head1 = SimHead(finger1, vicon_name='solo12', with_sliders=False)
-    head_dict = {'finger0': head0, 'finger1': head1}
+    # setup safety controllers to hold still when error occur
+    safety_pd_controller0 = HoldPDController(
+        head0, 3., 0.05, with_sliders=False)
+    safety_pd_controller1 = HoldPDController(
+        head1, 3., 0.05, with_sliders=False)
+    safety_controllers = safety_pd_controller0 if FINGER0_ONLY else [
+        safety_pd_controller0, safety_pd_controller1]
 
-    thread_head = ThreadHead(
-        dt,  # dt.
-        # Safety controllers.
-        [HoldPDController(head0, 4, .4), HoldPDController(head1, 4, .4)],
-        head_dict,  # Heads to read / write from.
-        [     # Utils.
-            # ('vicon', SimVicon(['solo12/solo12']))  # not using it
-        ],
-        bullet_env  # Environment to step.
-    )
+    # setup run controllers for running
+    ctrl0 = choose_controller(0, FINGER0_CONTROLLER, head0, ID0)
+    ctrl1 = choose_controller(1, FINGER1_CONTROLLER, head1, ID1)
+    controllers = ctrl0 if FINGER0_ONLY else [ctrl0, ctrl1]
 
-    # setup controller
-    ctrl0 = choose_controller(0, finger0_controller, head0, id0)
-    ctrl1 = choose_controller(1, finger1_controller, head1, id1)
-
-    try:
-        # start simulation
-        thread_head.switch_controllers((ctrl0, ctrl1))
-
-        # thread_head.start_streaming()
-        # thread_head.start_logging()
-
-        # thread_head.sim_run(int(sim_time/dt), sleep=True)
-        thread_head.start()
-        thread_head.join()
-
-        # thread_head.stop_streaming()
-        # thread_head.stop_logging()
-
-        # Plot timing information.
-        thread_head.plot_timing()
-    except KeyboardInterrupt:
-        print('\n', '-'*50, sep='')
-        print('ctrl c pressed, apply PD control to turn robot to origin pos')
-        print('-'*50)
-        thread_head.run_loop = False
-
-    # set up end PD control
-    end_ctrl = PDControl(PD_0, PD_1)
-    for _ in range(1000):
-        for head in head_dict.values():
-            head.read()
-            q = head.get_sensor('joint_positions')
-            dq = head.get_sensor('joint_velocities')
-            tau = end_ctrl.cal_torque(
-                np.array([0.]*3), q, np.array([0.]*3), dq)
-            head.set_control('ctrl_joint_torques', tau)
-            head.write()
-
-        # Step the actual simulation.
-        bullet_env.step(sleep=True)
-
-    print('-'*10, 'exit', '-'*10)
-
-
-def main_real(num=2, finger0_controller=0, finger1_controller=0):
-    # setup some parameters that might not be used?
-    dt = 0.001
-    id0 = 'finger0_lower_to_tip_joint'
-    id1 = 'finger1_lower_to_tip_joint'
-
-    # Create the dgm communication and instantiate the controllers.
-    path0 = os.path.join(NYUFingerDoubleConfig0().dgm_yaml_dir,
-                         'dgm_parameters_nyu_finger_double_0.yaml')
-    path1 = os.path.join(NYUFingerDoubleConfig1().dgm_yaml_dir,
-                         'dgm_parameters_nyu_finger_double_1.yaml')
-    head0 = dynamic_graph_manager_cpp_bindings.DGMHead(path0)
-    head1 = dynamic_graph_manager_cpp_bindings.DGMHead(path1)
-    head0.read()
-    head1.read()
-
-    # Create the safety controllers.
-    hold_pd_controller0 = HoldPDController(head0, 3., 0.05, with_sliders=False)
-    hold_pd_controller1 = HoldPDController(head1, 3., 0.05, with_sliders=False)
-    holders = hold_pd_controller0 if num!=2 else [hold_pd_controller0, hold_pd_controller1]
-    head_dict = head0 if num!=2 else {'finger0': head0, 'finger1': head1}
-
-
-    # setup controller
-    ctrl0 = choose_controller(0, finger0_controller, head0, id0)
-    ctrl1 = choose_controller(1, finger1_controller, head1, id1)
-    controllers = ctrl0 if num!=2 else [ctrl0, ctrl1]
+    # setup hold controllers for the end
+    des_angle = np.zeros(3)
+    end_ctrl0 = PDController(head0, ID0, PD_0, PD_1, des_angle)
+    end_ctrl1 = PDController(head1, ID1, PD_0, PD_1, des_angle)
+    end_controllers = [end_ctrl0, end_ctrl1]
 
     # setup thread_head
     thread_head = ThreadHead(
-        dt,
-        holders,
+        DT,
+        safety_controllers,
         head_dict,
-        []
+        []  # utils, no for now
     )
 
+    # call this function in ipython if try to stop the run
+    def stop(wait_time=1):
+        print('\n', '-'*50, sep='')
+        print('Apply PD control to turn robot to origin pos')
+        print('-'*50)
+
+        # Switch to end_controller
+        thread_head.switch_controllers(end_controllers)
+
+        # run for a bit and then stop
+        time.sleep(wait_time)
+        thread_head.run_loop = False
+
+        # set control torque to 0
+        for head in head_dict.values():
+            head.set_control('ctrl_joint_torques', np.array([0.]*3))
+            head.write()
+        print('-'*10, 'exit', '-'*10)
+        if SIMULATION:
+            thread_head.plot_timing()
+        # sys.exit(0)  # this raise error in ipython
 
     # Start the parallel processing.
     thread_head.switch_controllers(controllers)
-    try:
-        thread_head.start()
-        thread_head.join()
-    except KeyboardInterrupt:
-        print('\n', '-'*50, sep='')
-        print('ctrl c pressed, apply PD control to turn robot to origin pos')
-        print('-'*50)
-        thread_head.run_loop = False
+    thread_head.start()
 
-    # set up end PD control
-    end_ctrl = PDControl(PD_0, PD_1)
-    for _ in range(1000):
-        for head in head_dict.values():
-            head.read()
-            q = head.get_sensor('joint_positions')
-            dq = head.get_sensor('joint_velocities')
-            tau = end_ctrl.cal_torque(
-                np.array([0.]*3), q, np.array([0.]*3), dq)
-            head.set_control('ctrl_joint_torques', tau)
-            head.write()
-            time.sleep(dt)
-
-    print('-'*10, 'exit', '-'*10)
-
-
-if __name__ == '__main__':
-
-    main_sim(8, 1, 1)
-    # main_real(1, 1, 1)
+    # logging syntax in ipython: thread_head.start_logging(); time.sleep(1); thread_head.stop_logging()
