66e3f3d70ed11b8de8e2d195002da3948ae4b0db
Jerry <jerrywuzijie@gmail.com>
2021-07-21 12:48:55 -0400

    bash command script for initialize path variable

---
+++ demos/bash_command/setup_real_connection.bash
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+# remeber to run 'sudo -s' before running this bash
+# use 'source make_root_shell.bash' to set up
+
+source ~/Projects/finger_skills/demos/bash_command/setup_real_robot.bash
+
+cd ~/Projects/workspace/src/nyu_finger/demos/
+ipython
+
+# in ipython run nyu_finger_double_hwp.py
+
+# link for more info https://github.com/machines-in-motion/machines-in-motion.github.io/wiki/nyu_finger_hardware
---
+++ demos/bash_command/setup_real_vscode.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+#source ~/anaconda3/etc/profile.d/conda.sh
+#conda activate robot
+#source ~/setup.bash
+
+echo 'only used to open vscode, run in non-sudo mode'
+source ~/Projects/finger_skills/demos/bash_command/setup_real_robot.bash
+
+code ~/Projects
+cd ~/Projects/finger_skills/demos
