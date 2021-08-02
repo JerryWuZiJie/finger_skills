f4fe245056d3853d649c4aa52475b1211c1c19ba
Jerry Wu <jerrywuzijie@gmail.com>
2021-08-02 09:49:40 -0400

    working version while some bug might exists

--- src/finger_skills/eval_policy.py
+++ src/finger_skills/eval_policy.py
@@ -6,106 +6,90 @@
 	relying on ppo.py.
 """
 
-import numpy as np
+def _log_summary(ep_len, ep_ret, ep_num):
+		"""
+			Print to stdout what we've logged so far in the most recent episode.
+			Parameters:
+				None
+			Return:
+				None
+		"""
+		# Round decimal places for more aesthetic logging messages
+		ep_len = str(round(ep_len, 2))
+		ep_ret = str(round(ep_ret, 2))
 
+		# Print logging statements
+		print(flush=True)
+		print(f"-------------------- Episode #{ep_num} --------------------", flush=True)
+		print(f"Episodic Length: {ep_len}", flush=True)
+		print(f"Episodic Return: {ep_ret}", flush=True)
+		print(f"------------------------------------------------------", flush=True)
+		print(flush=True)
 
-def _log_summary(batch_lens, batch_rets, total_ep):
-    """
-            Print to stdout what we've logged so far in the most recent episode.
+def rollout(policy, env, render):
+	"""
+		Returns a generator to roll out each episode given a trained policy and
+		environment to test on. 
+		Parameters:
+			policy - The trained policy to test
+			env - The environment to evaluate the policy on
+			render - Specifies whether to render or not
+		
+		Return:
+			A generator object rollout, or iterable, which will return the latest
+			episodic length and return on each iteration of the generator.
+		Note:
+			If you're unfamiliar with Python generators, check this out:
+				https://wiki.python.org/moin/Generators
+			If you're unfamiliar with Python "yield", check this out:
+				https://stackoverflow.com/questions/231767/what-does-the-yield-keyword-do
+	"""
+	# Rollout until user kills process
+	while True:
+		obs = env.reset()
+		done = False
 
-            Parameters:
-                    None
+		# number of timesteps so far
+		t = 0
 
-            Return:
-                    None
-    """
-    # calculate average
-    batch_lens = np.mean(batch_lens)
-    batch_rets = np.mean(batch_rets)
+		# Logging data
+		ep_len = 0            # episodic length
+		ep_ret = 0            # episodic return
 
-    # Round decimal places for more aesthetic logging messages
-    batch_lens = str(round(batch_lens, 2))
-    batch_rets = str(round(batch_rets, 2))
+		while not done:
+			t += 1
 
-    # Print logging statements
-    print(flush=True)
-    print(
-        f"-------------------- Total Episode #{total_ep} --------------------", flush=True)
-    print(f"Avg Episodic Length: {batch_lens}", flush=True)
-    print(f"Avg Episodic Return: {batch_rets}", flush=True)
-    print(f"------------------------------------------------------", flush=True)
-    print(flush=True)
+			# Render environment if specified, off by default
+			if render:
+				env.render()
 
+			# Query deterministic action from policy and run it
+			action = policy(obs).detach().numpy()
+			obs, rew, done, _ = env.step(action)
 
-def rollout(policy, env, episode, render):
-    """
-        Returns a generator to roll out each episode given a trained policy and
-        environment to test on. 
+			# Sum all episodic rewards as we go along
+			ep_ret += rew
+			
+		# Track episodic length
+		ep_len = t
 
-        Parameters:
-                policy - The trained policy to test
-                env - The environment to evaluate the policy on
-                render - Specifies whether to render or not
+		# returns episodic length and return in this iteration
+		yield ep_len, ep_ret
 
-        Return:
-                TODO
-    """
-    batch_lens = []
-    batch_rews = []
-
-    # Rollout
-    for _ in range(episode):
-        # print one * for each episode
-        if _ % 50 == 0:
-            # change line
-            print()
-        print('*', end='', flush=True)
-
-        obs = env.reset()
-        done = False
-
-        # Logging data
-        ep_len = 0            # episodic length
-        ep_ret = 0            # episodic return
-
-        while not done:
-        # Track episodic length
-            ep_len += 1
-
-            # Render environment if specified, off by default
-            if render:
-                env.render()
-
-            # Query deterministic action from policy and run it
-            action = policy(obs).detach().numpy()
-            obs, rew, done, _ = env.step(action)
-
-            # Sum all episodic rewards as we go along
-            ep_ret += rew
-
-        # returns episodic length and return in this iteration
-        batch_lens.append(ep_len)
-        batch_rews.append(ep_ret)
-
-    return batch_lens, batch_rews
-
-
-def eval_policy(policy, env, episode=10, render=False):
-    """
-            The main function to evaluate our policy with. It will iterate a generator object
-            "rollout", which will simulate each episode and return the most recent episode's
-            length and return. We can then log it right after. And yes, eval_policy will run
-            forever until you kill the process. 
-
-            Parameters:
-                    policy - The trained policy to test, basically another name for our actor model
-                    env - The environment to test the policy on
-                                        episode - Number of games to run
-                    render - Whether we should render our episodes. False by default.
-
-            Return:
-                    None
-    """
-    # Rollout with the policy and environment, and log each episode's data
-    batch_lens, batch_rews = rollout(policy, env, episode, render)
-    _log_summary(batch_lens, batch_rews, episode)
+def eval_policy(policy, env, render=False):
+	"""
+		The main function to evaluate our policy with. It will iterate a generator object
+		"rollout", which will simulate each episode and return the most recent episode's
+		length and return. We can then log it right after. And yes, eval_policy will run
+		forever until you kill the process. 
+		Parameters:
+			policy - The trained policy to test, basically another name for our actor model
+			env - The environment to test the policy on
+			render - Whether we should render our episodes. False by default.
+		Return:
+			None
+		NOTE: To learn more about generators, look at rollout's function description
+	"""
+	# Rollout with the policy and environment, and log each episode's data
+	for ep_num, (ep_len, ep_ret) in enumerate(rollout(policy, env, render)):
+		_log_summary(ep_len=ep_len, ep_ret=ep_ret, ep_num=ep_num)
--- src/finger_skills/main.py
+++ src/finger_skills/main.py
@@ -15,6 +15,21 @@ import eval_policy
 import env_finger
 
 
+DT = 0.01
+MAX_TIMESTEPS_PER_EPISODE = int(20/DT)
+TIMESTEPS_PER_BATCH = MAX_TIMESTEPS_PER_EPISODE * 20
+MODE = 0
+
+RENDER = False
+if MODE == 0:
+    MODE = 'train'
+elif MODE == 1:
+    MODE = 'test'
+    RENDER = True
+elif MODE == 2:
+    MODE = 'restart'
+
+
 def train(env, args):
     print(f"Training")
 
@@ -85,14 +100,14 @@ def test(env, args):
     # ppo.py since it only contains the training algorithm. The model/policy itself exists
     # independently as a binary file that can be loaded in with torch.
     eval_policy.eval_policy(
-        policy=policy, env=env, episode=args.iteration, render=args.hyperparameters['render'])
+        policy=policy, env=env, render=args.hyperparameters['render'])
 
     print('\n\nTesting done')
 
 
 def main(args):
     # make environment and model
-    env = env_finger.EnvFingers(render=args.hyperparameters['render'])
+    env = env_finger.EnvFingers(render=args.hyperparameters['render'], dt=DT)
 
     if args.mode != 'test':
         train(env, args)
@@ -105,19 +120,19 @@ def main(args):
 if __name__ == '__main__':
     class Temp:
         hyperparameters = {
-            'timesteps_per_batch': 100_000,
-            'max_timesteps_per_episode': 20_000,
+            'timesteps_per_batch': TIMESTEPS_PER_BATCH,
+            'max_timesteps_per_episode': MAX_TIMESTEPS_PER_EPISODE,
             'gamma': 0.99,
             'n_updates_per_iteration': 5,
             'lr': 3e-4,
             'clip': 0.2,
-            'render': False,
+            'render': RENDER,
             'render_every_i': 1,
-            'save_freq': 10,
+            'save_freq': 1,
             'seed': 0,
         }
 
-        mode = 'train'  # train/restart/test
+        mode = MODE  # train/restart/test
         iteration = 10  # iteration in train iterate through one batch, iteration in test iterate through one game
 
         actor_model = '/home/jerry/Projects/finger_skills/src/finger_skills/model_state_dict/ppo_actor.pth'
